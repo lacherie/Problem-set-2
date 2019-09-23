cb106a7c92383028516faf412c2fefd8ace7db42
lacherie <55232600+lacherie@users.noreply.github.com>
2019-09-23 17:08:15 +0200

    Update hangmanpart3.py

--- hangmanpart3.py
+++ hangmanpart3.py
@@ -51,7 +51,6 @@ def is_word_guessed(secret_word, letters_guessed):
     returns: boolean, True if all the letters of secret_word are in letters_guessed;
       False otherwise
     '''
-    # FILL IN YOUR CODE HERE AND DELETE "pass"
     answer = True
     for char in secret_word:
         if char in letters_guessed:
